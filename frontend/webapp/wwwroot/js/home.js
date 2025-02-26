ad196df724ad4885ee1a77daf6a5b530e494ee67
Lucas Valverde <lucas.valverde@pathbit.com.br>
2025-02-25 22:05:38 -0300

    checkin a cada 30 seg

--- frontend/webapp/wwwroot/js/home.js
+++ frontend/webapp/wwwroot/js/home.js
@@ -5,4 +5,11 @@ window.onload = () => {
     if (!checkToken()) {
         window.location.href = 'login.html'; // Redirecionar para login.html se o token não estiver presente
     }
+
+    // Verificar periodicamente se o token expirou ou foi removido
+    setInterval(() => {
+        if (!checkToken()) {
+            window.location.href = 'login.html'; // Redirecionar para login.html se o token expirar ou for removido
+        }
+    }, 30000); // Verificar a cada 30 segundos
 };
